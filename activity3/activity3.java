f35824147bd9ed03b0ab6658675314992f90c27f
austinhucabee <ahucabee@gmail.com>
2021-09-10 12:01:02 -0500

    initial commit - moving project

---
+++ activity3/activity3.java
@@ -0,0 +1,7 @@
+public class activity3 {
+
+    public static void main(String [] args) {
+        System.out.println("Hello. This is sample code for activity 3");
+        System.out.println("hi");
+    }
+}
